c7230cf090ebc20132a050659172e9a696c0f805
jacebright <jacebright@gmail.com>
2024-11-16 09:49:25 -0600

    queue tests

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,85 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    // Scenario: Given the values in the order of priority, ensure they are dequeued in that order
+    // This is testing the enqueue and dequeue functionalities.
+    // Expected Result: "John", "Jacob", "Jingle", "Heimer", "Schmidt"
+    // Defect(s) Found: The index was starting at 1 in the dequeue method, and was not removing the item
+    public void TestPriorityQueue_PrioritiesInOrder()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("John", 5);
+        priorityQueue.Enqueue("Jacob", 4);
+        priorityQueue.Enqueue("Jingle", 3);
+        priorityQueue.Enqueue("Heimer", 2);
+        priorityQueue.Enqueue("Schmidt", 1);
+
+        List<string> expected = ["John", "Jacob", "Jingle", "Heimer", "Schmidt"];
+
+
+        for (int i = 0; i < 5; i++) {
+            string result = priorityQueue.Dequeue();
+            Assert.AreEqual(expected[i], result);
+        }
+
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_2()
+    // Scenario: Enqueue the values in the incorrect order to confirm the highest priority is being used.
+    // Expected Result: "John", "Jacob", "Jingle", "Heimer", "Schmidt"
+    // Defect(s) Found: none
+    public void TestPriorityQueue_PrioritiesOutOfOrder()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("Schmidt", 1);
+        priorityQueue.Enqueue("Jingle", 3);
+        priorityQueue.Enqueue("John", 5);
+        priorityQueue.Enqueue("Jacob", 4);
+        priorityQueue.Enqueue("Heimer", 2);
+        
+
+        List<string> expected = ["John", "Jacob", "Jingle", "Heimer", "Schmidt"];
+
+
+        for (int i = 0; i < 5; i++) {
+            string result = priorityQueue.Dequeue();
+            Assert.AreEqual(expected[i], result);
+        }
     }
 
-    // Add more test cases as needed below.
+    [TestMethod]
+    // Scenario: Enqueue the values with matching priorities to ensure the one with the lowest
+    // index is being used.
+    // Expected Result: "John", "Jacob", "Jingle", "Heimer", "Schmidt"
+    // Defect(s) Found: The highest priority index was checking for greater than or equal to
+    // instead of just greater than.
+    public void TestPriorityQueue_EqualValuePriorities()
+    {
+        var priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue("Schmidt", 1);
+        priorityQueue.Enqueue("Jingle", 4);
+        priorityQueue.Enqueue("John", 5);
+        priorityQueue.Enqueue("Jacob", 5);
+        priorityQueue.Enqueue("Heimer", 4);
+        
+
+        List<string> expected = ["John", "Jacob", "Jingle", "Heimer", "Schmidt"];
+
+
+        for (int i = 0; i < 5; i++) {
+            string result = priorityQueue.Dequeue();
+            Assert.AreEqual(expected[i], result);
+        }
+    }
+
+    [TestMethod]
+    // Scenario: attempt to dequeue a value from an empty queue
+    // Expected Result: invalid operation exception
+    // Defect(s) Found: none
+    public void TestPriorityQueue_ErrorCatching()
+    {
+        var priorityQueue = new PriorityQueue();
+        
+        Assert.ThrowsException<InvalidOperationException>( () => priorityQueue.Dequeue()); 
+    }
 }
