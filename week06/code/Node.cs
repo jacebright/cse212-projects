08d942e5ecd444a8362562de82c89df4ef1c45c9
jacebright <jacebright@gmail.com>
2024-12-11 23:19:49 -0600

    weekly activities

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Node
 {
     public int Data { get; set; }
@@ -18,6 +20,8 @@ public class Node
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
+            else if (Left.Data == value)
+                Console.WriteLine("The value already exists");
             else
                 Left.Insert(value);
         }
@@ -26,6 +30,8 @@ public class Node
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
+            else if (Right.Data == value)
+                Console.WriteLine("The value already exists");
             else
                 Right.Insert(value);
         }
@@ -33,13 +39,34 @@ public class Node
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data){
+            return true;
+        }
+        else if (value < Data){
+            if (Left is null)
+                return false;
+            else
+                return Left.Contains(value);
+        }
+        else {
+            if (Right is null)
+                return false;
+            else
+                return Right.Contains(value);
+        }
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        if (Left is null && Right is null)
+            return 1;
+        else
+            if (Left is not null && Right is not null)
+                return 1 + +Math.Max(Left.GetHeight(), Right.GetHeight());
+            else if (Left is null && Right is not null)
+                return 1 + Right.GetHeight();
+            else if (Right is null && Left is not null)
+                return 1 + Left.GetHeight();
+            else return 0;
     }
 }
